049bbbf111aa5592f9176f895a7eec03c68efb34
Trung Lê Hoàng <trunglh@pmr.vn>
2024-03-20 18:29:09 +0700

    Add project files.

---
+++ Domain/Migrations/20240320081612_InitApp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitApp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(255)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(255)", nullable: true),
+                    Content = table.Column<string>(type: "nvarchar(255)", nullable: true),
+                    Sku = table.Column<string>(type: "nvarchar(255)", nullable: true),
+                    Price = table.Column<decimal>(type: "money", nullable: false),
+                    DiscountPrice = table.Column<double>(type: "float", nullable: true),
+                    ImageUrl = table.Column<string>(type: "varchar(255)", nullable: true),
+                    ImageList = table.Column<string>(type: "varchar(255)", nullable: true),
+                    ViewCount = table.Column<int>(type: "int", nullable: true),
+                    SeoAlias = table.Column<string>(type: "nvarchar(255)", nullable: true),
+                    SeoTitle = table.Column<string>(type: "nvarchar(255)", nullable: true),
+                    SeoKeyword = table.Column<string>(type: "nvarchar(255)", nullable: true),
+                    SeoDescription = table.Column<string>(type: "varchar(255)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: true),
+                    RateTotal = table.Column<int>(type: "int", nullable: true),
+                    RateCount = table.Column<int>(type: "int", nullable: true),
+                    ObjId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ Domain/Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Models
+{
+    public class Product : BaseModel
+    {
+        [Column(TypeName = "nvarchar(255)")]
+        public string? Name { get; set; }
+        [Column(TypeName = "nvarchar(255)")]
+        public string? Description { get; set; }
+        [Column(TypeName = "nvarchar(255)")]
+        public string? Content { get; set; }
+        [Column(TypeName = "nvarchar(255)")]
+        public string? Sku { get; set; }
+        [Column(TypeName = "money")]
+        public decimal? Price { get; set; }
+        [Column(TypeName = "float")]
+        public float? DiscountPrice { get; set; }
+        [Column(TypeName = "varchar(255)")]
+        public string? ImageUrl { get; set; }
+        [Column(TypeName = "varchar(255)")]
+        public string? ImageList { get; set; }
+        [Column(TypeName = "int")]
+        public int? ViewCount { get; set; }
+        [Column(TypeName = "nvarchar(255)")]
+        public string? SeoAlias { get; set; }
+        [Column(TypeName = "nvarchar(255)")]
+        public string? SeoTitle { get; set; }
+        [Column(TypeName = "nvarchar(255)")]
+        public string? SeoKeyword { get; set; }
+        [Column(TypeName = "varchar(255)")]
+        public string? SeoDescription { get; set; }
+        [Column(TypeName = "bit")]
+        public bool? IsActive { get; set; }
+        [Column(TypeName = "int")]
+        public int? RateTotal { get; set; }
+        [Column(TypeName = "int")]
+        public int? RateCount { get; set; }
+    }
+}
